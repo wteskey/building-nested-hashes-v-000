3c354e65c5a41fca37bb44335bf4a58ac4af5dce
William J Teskey <willjteskey@gmail.com>
2018-04-17 19:00:41 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch =>
+      :matriarch =>
+      :heroine =>
+      :heroine_friends =>
       }, 
     :capulet => {
       #your key/value pairs here!
