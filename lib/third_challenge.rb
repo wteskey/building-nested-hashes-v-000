fa6320b3c758d4c1b2ece57ab63c21d13149b20a
William J Teskey <willjteskey@gmail.com>
2018-04-17 20:07:32 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,26 +2,29 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => { 
-        :name => "lord Montague", 
-        :age => 53
+        name: "lord Montague", 
+        age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Montague", 
+        age: 54 
         },
       :hero => {
-        #your key/value pairs here
-        },
+        name: "Romeo", 
+        age: "15"
+        status: "alive"
+       },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet", age: "15", satus: "alive"
         },
       :heroine_friends => []
    }
